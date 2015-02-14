77e7c25eae84ea26d93fda351d5c9b32cd96d6bc
Marifel <mrbarbasa@gmail.com>
2015-02-13 22:47:49 -1000

    Implement codepen working Slide Box sample

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('ionicApp', ['ionic', 'ionicApp.controllers'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -17,3 +17,14 @@ angular.module('starter', ['ionic'])
     }
   });
 })
+
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+  .state('intro', {
+    url: '/',
+    templateUrl: 'intro.html',
+    controller: 'IntroCtrl'
+  });
+
+  $urlRouterProvider.otherwise("/");
+});
