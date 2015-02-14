a7a810223fdb93e5d6854757af3da383a75fc72e
dmeowmixer <dmeowmixer@hotmail.com>
2015-02-13 22:47:44 -1000

    Finished mock datas for clothing items

---
+++ www/data/data.js
@@ -0,0 +1,128 @@
+var foodopt = [{
+  id : 0,
+  cuisine : 'Mexican',
+  location : 'Kakaako',
+  name : 'Cocina',
+  cost : '$$'
+}, {
+  id : 1,
+  cuisine : 'Japanese',
+  location : 'Ward',
+  name : 'Nanzan GiroGiro',
+  cost : '$$$'
+}, {
+  id : 2, 
+  cuisine : 'Dimsum',
+  location : 'Chinatown',
+  name : 'Mei Sum',
+  cost : '$'
+}, {
+  id : 3,
+  cuisine : 'Korean',
+  location : 'Kalihi',
+  name : 'HanYang',
+  cost : '$'
+}, {
+  id : 4,
+  cuisine : 'Thai',
+  location : 'Liliha',
+  name : 'Thai Cafe',
+  cost : '$'
+}, {
+  id : 5,
+  cuisine : 'Burgers', 
+  location : 'Kakaako',
+  name : 'Kua"aina burger',
+  cost : '$$'
+}];
+
+
+
+
+var clothopt = [{
+  id : 13,
+  type : 'Casual',
+  color : ' Beige',
+  length : 'Short',
+  item : 'Cargo Shorts'
+}, {
+  id : 0,
+  type : 'Casual',
+  color : 'Black',
+  length : 'Long',
+  item : 'Tank-top'  
+}, {
+  id : 1,
+  type : 'Casual',
+  color: 'Grey',
+  length: 'Long',
+  item : 'Sweatpants'
+}, {
+  id : 2,
+  type : 'Casual',
+  color : 'White',
+  length : null,
+  item : 'Sneakers'
+}, {
+  id : 3,
+  type : 'Casual',
+  color : 'Black',
+  length : 'Short',
+  item : 'Pants'
+}, {
+  id : 4,
+  type : 'Casual',
+  color : 'Black',
+  length : null,
+  item : 'Slippers'
+}, {
+  id : 5, 
+  type : 'Business', 
+  color : 'Blue',
+  length : 'Long',
+  item : 'Dress-Shirt'
+}, {
+  id : 6,
+  type : 'Business',
+  color: 'Black',
+  length : 'Long',
+  item : 'Slacks'
+}, {
+  id :12,
+  type : 'Business',
+  color : 'Black',
+  length : null,
+  item : null,
+  accessory : 'Fancy NeckTie'
+},{
+  id : 7,
+  type : 'Business',
+  color: 'Black',
+  length : null,
+  item : 'Dress-Shoes' 
+}, {
+  id : 8,
+  type : 'Hot Date',
+  color: 'Pink',
+  length : 'Long',
+  item : 'Dress-Shirt'
+}, {
+  id : 9, 
+  type : 'Hot Date',
+  color : 'Black',
+  length : 'Long',
+  item : 'Slacks'
+}, { 
+  id :10,
+  type : 'Hot Date',
+  color : 'Black',
+  length : null,
+  item : 'Dress-Shoes'
+}, {
+  id: 11,
+  type : 'Hot Date',
+  color : 'White',
+  length : null,
+  item : null,
+  accessory : 'Fancy NeckTie'
+}];
